6d64e0e825c65926dff94a64562b3e58849c2745
donghyeon choi <downpool.choi@gmail.com>
2025-03-10 19:16:54 +0900

    iOS init

---
+++ NFCRFID/NFCRFID/NFCRFIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCRFIDApp.swift
+//  NFCRFID
+//
+//  Created by donghyeon choi on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NFCRFIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
